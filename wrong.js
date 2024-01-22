82372cafe6db09ea451e2a97696c757823b68286
CarinWood <carin.wood.85@gmail.com>
2024-01-22 13:16:24 +0100

    header flex property changed

--- wrong.js
+++ wrong.js
@@ -3,3 +3,10 @@ Change it to the correct one. */
 
 let text = document.querySelector('.logo-text')
 text.style.color = 'black'
+
+/* The alignment of the elements inside the header element are wrong. 
+Change it to the correct one. 
+Hint, check the flex properties for the correct alignment. */
+
+let header = document.querySelector('header');
+header.style.justifyContent = 'flex-start'
