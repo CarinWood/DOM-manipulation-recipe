5cfe0538eea2e699ff8cae53fead0ab2930251c5
CarinWood <carin.wood.85@gmail.com>
2024-01-22 12:10:31 +0100

    update

--- index.js
+++ index.js
@@ -13,4 +13,31 @@ console.log(window.getComputedStyle(description).fontSize)
 
 /* What is the value of the alt atrribute on the image? */
 let image = document.querySelector('img')
-console.log()
+console.log(image.getAttribute('alt'))
+
+/* What is the dimensions and the url of the image? Create an object that looks like this, and log it to the console:
+{
+  url: string
+  height: number,
+  width: number,
+} */
+
+let imageObject = {
+    url: image.getAttribute('src'),
+    height: image.height,
+    width: image.width
+}
+
+for (let item in imageObject) {
+       console.log(item + ":" + imageObject[item])
+}
+/* 
+How many ingredients has the paste? */
+
+let ingredients = document.querySelector('.ingredients-list-paste')
+console.log(ingredients.children.length)
+
+/* Which is the forth element in the list containing the 
+ingredients for the paste? */
+
+console.log(ingredients.children[3].innerText)
